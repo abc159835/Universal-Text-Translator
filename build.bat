35a8305eb2e99a5c7767c6d3868d92723a584386
abc159835 <abc159835@163.com>
2023-05-27 13:56:49 +0800

    ignored

--- build.bat
+++
@@ -1,2 +0,0 @@
-cd vue
-yarn build && pause && cd .. && xcopy "vue\dist" "dist\Application\static" /E /I /H /Y && pause
--- package.bat
+++
@@ -1,3 +0,0 @@
-chcp 65001
-pyinstaller --windowed main.py --name "Application" --add-data "./vue/dist;static/" --icon "./vue/dist/toolbox.ico" --noconfirm
-pause
