edee2fcb34155579a67e793b1849a52777d45954
abc159835 <abc159835@163.com>
2023-05-28 11:19:15 +0800

    dev

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from module.network import app
+from module.env import env
 from loguru import logger
 import module.OShelper
 import webview
@@ -12,8 +13,11 @@ def open_folder():
     else:
         return None
 
+if env():
+    window = webview.create_window('Universal Translator',url='http://localhost:5173/',width=2160,height=1200)
+else:
+    window = webview.create_window('Universal Translator',url=app,width=2160,height=1200)
 
-window = webview.create_window('Universal Translator',url=app,width=2160,height=1200)
 window.expose(module.OShelper.get_folder_all_file,open_folder,module.OShelper.get_file_content)
 
 # MainThread blocked
